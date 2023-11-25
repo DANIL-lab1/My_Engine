78e6062cd562cc51c0679bbd8059c34543152f3b
DANIL-lab1 <65044715+DANIL-lab1@users.noreply.github.com>
2023-11-25 19:06:36 +0300

    Add OpenGl library as GLAD, Checked, if it is worked

--- MyEngineCore/src/MyEngineCore/Application.cpp
+++ MyEngineCore/src/MyEngineCore/Application.cpp
@@ -2,6 +2,8 @@
 
 #include "MyEngineCore/Log.hpp"
 
+#include <glad/glad.h>
+
 #include <GLFW/glfw3.h>
 
 #include "spdlog/spdlog.h"
@@ -44,11 +46,19 @@ namespace MyEngine {
         /* Make the window's context current */
         glfwMakeContextCurrent(window);
 
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+            LOG_CRITICAL("Failed to initialize GLAD");
+            return -1;
+        }
+
+        glClearColor(1, 0, 0, 0);
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            //glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT);
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
