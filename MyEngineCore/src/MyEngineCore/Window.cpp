5b3d3f72c2b618e15f42247e876c6694a74c5bf8
DANIL-lab1 <65044715+DANIL-lab1@users.noreply.github.com>
2023-11-27 14:04:57 +0300

    Start working with graphics. Draw triangle as a start

--- MyEngineCore/src/MyEngineCore/Window.cpp
+++ MyEngineCore/src/MyEngineCore/Window.cpp
@@ -16,6 +16,55 @@ namespace MyEngine {
     // Переменная инициализация
     static bool s_GLFW_initialized = false;
 
+    // Массив с позициями треугольника
+    GLfloat points[] = {
+       0.0f,  0.5f, 0.0f,
+       0.5f, -0.5f, 0.0f,
+      -0.5f, -0.5f, 0.0f
+    };
+
+    // Массив с цветом каждой вершины треугольника
+    GLfloat colors[] = {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
+    };
+
+    // Вертексный шейдер (трасформация координат и вычисления связанные с ними) 
+    const char* vertex_shader =
+        // Версия шейдера
+        "#version 460\n"
+        // Входные данные шейдера (позиция и цвет)
+        "layout(location = 0) in vec3 vertex_position;"
+        "layout(location = 1) in vec3 vertex_color;"
+        // Выходные данные шейдера (векторы)
+        "out vec3 color;"
+        // Функция для цвета
+        "void main() {"
+            // Задаём цвет вертекса
+        "   color = vertex_color;"
+            // Задаём позицию вертекса (4 параметр - перспектива)
+        "   gl_Position = vec4(vertex_position, 1.0);"
+        "}";
+
+    // Фрагментный шейдер
+    const char* fragment_shader =
+        // Версия шейдера
+        "#version 460\n"
+        // Входные данные (цвет)
+        "in vec3 color;"
+        // Выходные данные (итоговый цвет)
+        "out vec4 frag_color;"
+        // Функция для задания цвета
+        "void main() {"
+            // Задаём цвет (4 параметр - прозрачность)
+        "   frag_color = vec4(color, 1.0);"
+        "}";
+
+    // Две переменные для шейдеров (хранения и обработки)
+    GLuint shader_program;
+    GLuint vao;
+
     // Конструктор и запуск игрового движка
 	Window::Window(std::string title, const unsigned int width, const unsigned int height)
         : m_data({ std::move(title), width, height }) {
@@ -100,6 +149,63 @@ namespace MyEngine {
             data.eventCallbackFn(event);
         });
 
+        // Задаём границы отрисовки экрана для треугольника
+        glfwSetFramebufferSizeCallback(m_pWindow,
+            [](GLFWwindow* pWindow, int width, int height)
+            {
+                glViewport(0, 0, width, height);
+            });
+
+        // Вертексный шейдер (создание, задание и инициализация его в проекте)
+        GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vs, 1, &vertex_shader, nullptr);
+        glCompileShader(vs);
+
+        // Фрагментный шейдер (создание, задание и инициализация его в проекте)
+        GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fs, 1, &fragment_shader, nullptr);
+        glCompileShader(fs);
+
+        // Обработка шейдеров с помощью шейдерной программы (инициализация программы, обработка двух шейдеров и создание итоговой программы) 
+        shader_program = glCreateProgram();
+        glAttachShader(shader_program, vs);
+        glAttachShader(shader_program, fs);
+        glLinkProgram(shader_program);
+
+        // Удаляем использованные шейдеры
+        glDeleteShader(vs);
+        glDeleteShader(fs);
+
+        // Два массива памяти для видеокарты
+
+        // Создаём буфер для ингерации в него позиции шейдеров
+        GLuint points_vbo = 0;
+        glGenBuffers(1, &points_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+        // Создаём буфер для ингерации в него цвета шейдеров
+        GLuint colors_vbo = 0;
+        glGenBuffers(1, &colors_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+        // Обработка значений
+
+        // Создаём вертекс для обработки наших значений шейдеров
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+
+        // Обработка (связка) шейдера позиции
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        // Обработка (связка) шейдера цвета
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
         return 0;
 	}
 
@@ -114,6 +220,11 @@ namespace MyEngine {
         // Делаем выборку цвета фона динамичной
         glClearColor(m_background_color[0], m_background_color[1], m_background_color[2], m_background_color[3]);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Отрисовка треугольника (подключение программы шейдеров, подключаем vertex array object (обработка), сама отрисовка треуголника)
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Задание размеров окна
         ImGuiIO& io = ImGui::GetIO();
